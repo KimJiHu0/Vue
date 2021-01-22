2c4bf8225a42c21dccc9e5cce578bee00dc00a5a
KimJiHu0 <sh9732626@gmail.com>
2021-01-22 14:54:26 +0900

    Vue : Project 구성 방법

---
+++ 08VueCLITemplate/frontend/src/components/Header.vue
@@ -0,0 +1,14 @@
+<!-- 기본 세팅 -->
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 08VueCLITemplate/src/test/java/com/spring/vue/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
